d6b96db2a437544498c367d5e1fee5bc427a4d45
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2025-08-01 22:27:59 +0800

    移除post-link脚本，更新构建配置以支持多平台构建，调整依赖项以适应不同操作系统。

--- .conda/scripts/post-link.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-# make sure pip is available
-"$PREFIX/bin/python" -m pip install --upgrade pip
-
-# install extra deps
-"$PREFIX/bin/python" -m pip install paho-mqtt opentrons_shared_data
-"$PREFIX/bin/python" -m pip install git+https://github.com/Xuwznln/pylabrobot.git
