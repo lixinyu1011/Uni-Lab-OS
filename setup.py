cfc589819a6f3071d5a72c15513dc994a261c5f4
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2025-08-02 19:18:06 +0800

    bump version & use new resource registry upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'unilabos'
 
 setup(
     name=package_name,
-    version='0.10.2',
+    version='0.10.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['setuptools'],
--- unilabos/app/register.py
+++ unilabos/app/register.py
@@ -18,28 +18,28 @@ def register_devices_and_resources(mqtt_client, lab_registry):
         mqtt_client.publish_registry(device_info["id"], device_info, False)
         logger.debug(f"[UniLab Register] 注册设备: {device_info['id']}")
 
-    # 注册资源信息
-    for resource_info in lab_registry.obtain_registry_resource_info():
-        mqtt_client.publish_registry(resource_info["id"], resource_info, False)
-        logger.debug(f"[UniLab Register] 注册资源: {resource_info['id']}")
-
-    # # 注册资源信息 - 使用HTTP方式
-    # from unilabos.app.web.client import http_client
-    #
-    # resources_to_register = {}
+    # # 注册资源信息
     # for resource_info in lab_registry.obtain_registry_resource_info():
-    #     resources_to_register[resource_info["id"]] = resource_info
-    #     logger.debug(f"[UniLab Register] 准备注册资源: {resource_info['id']}")
-    #
-    # if resources_to_register:
-    #     start_time = time.time()
-    #     response = http_client.resource_registry(resources_to_register)
-    #     cost_time = time.time() - start_time
-    #     if response.status_code in [200, 201]:
-    #         logger.info(f"[UniLab Register] 成功通过HTTP注册 {len(resources_to_register)} 个资源 {cost_time}ms")
-    #     else:
-    #         logger.error(f"[UniLab Register] HTTP注册资源失败: {response.status_code}, {response.text} {cost_time}ms")
-    # logger.info("[UniLab Register] 设备和资源注册完成.")
+    #     mqtt_client.publish_registry(resource_info["id"], resource_info, False)
+    #     logger.debug(f"[UniLab Register] 注册资源: {resource_info['id']}")
+
+    # 注册资源信息 - 使用HTTP方式
+    from unilabos.app.web.client import http_client
+
+    resources_to_register = {}
+    for resource_info in lab_registry.obtain_registry_resource_info():
+        resources_to_register[resource_info["id"]] = resource_info
+        logger.debug(f"[UniLab Register] 准备注册资源: {resource_info['id']}")
+
+    if resources_to_register:
+        start_time = time.time()
+        response = http_client.resource_registry(resources_to_register)
+        cost_time = time.time() - start_time
+        if response.status_code in [200, 201]:
+            logger.info(f"[UniLab Register] 成功通过HTTP注册 {len(resources_to_register)} 个资源 {cost_time}ms")
+        else:
+            logger.error(f"[UniLab Register] HTTP注册资源失败: {response.status_code}, {response.text} {cost_time}ms")
+    logger.info("[UniLab Register] 设备和资源注册完成.")
 
 
 def main():
