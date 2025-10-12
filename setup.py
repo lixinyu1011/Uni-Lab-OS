7c7f1b31c5962ced50cc5c4908de886db6ae676d
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2025-10-12 22:52:34 +0800

    Bump version to 0.10.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'unilabos'
 
 setup(
     name=package_name,
-    version='0.10.6',
+    version='0.10.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['setuptools'],
